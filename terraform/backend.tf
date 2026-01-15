433bab7e0c8ed543b5367d14121660710177601f
Donatas <ktp211zdv@yandex.ru>
2026-01-15 10:46:34 +0300

    change backend s3

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,9 @@ terraform {
   backend "s3" {
     bucket                      = "2d25c8ae-dev-terraform-state"
     key                         = "terraform.tfstate"
-    endpoint                    = "https://s3.twcstorage.ru"
+    endpoints = {
+      s3 = "https://s3.twcstorage.ru"
+    }
     region                      = "ru-1"
     skip_region_validation      = true
     skip_credentials_validation = true
